3c84522a9cf8055d7371c307f97a1c9b4b90b067
Sean Harmon <seanisloveseanislife@gmail.com>
2026-03-03 20:45:00 -0800

    functionalize intervention, check for do/cf consistency
    Checking for do and counter factual consistency by producing no-intervention + do pair and no-intervention + counter factual pair. Note that the counter factual is produced under the same method as no-intervention, just a new sampled is produced from the SCM
    Validated results by checking whether the intervention results in a significant enough distribution shift to not pass the KS test (or in the case of the counterfactual, whether the KS test fails to produce statistically significant results).

--- part_1/do_probability_estimates.py
+++ part_1/do_probability_estimates.py
@@ -6,18 +6,25 @@ import numpy as np
 
 np.random.seed(25)
 
-with open("generated_data/synthetic/synthetic_0_nodes_4.pkl", "rb") as f:
-    scm = pickle.load(f)
+def do_cf_intervention(scm, interventions, n_samples):
+    interventional_data = gcm.interventional_samples(
+        scm,
+        interventions=interventions,
+        num_samples_to_draw=n_samples
+    )
 
-interventional_data = gcm.interventional_samples(
-    scm,
+    return interventional_data
+
+if __name__ == "__main__":
+    with open("generated_data/synthetic/synthetic_0_nodes_4.pkl", "rb") as f:
+        scm = pickle.load(f)
     interventions={'X1': lambda x: x*1.1}, # do(X1=5)
-    num_samples_to_draw=10000
-)
+    n_samples = 100_000
+    interventional_data = do_cf_intervention(scm, interventions, n_samples)
 
-event_occurrences = (interventional_data['X2'] > 1).sum() # P(X2 > 1 | do(X1 = 1.1*X1))
-total_samples = len(interventional_data)
+    event_occurrences = (interventional_data['X2'] > 1).sum() # P(X2 > 1 | do(X1 = 1.1*X1))
+    total_samples = len(interventional_data)
 
-probability = event_occurrences / total_samples
+    probability = event_occurrences / total_samples
 
-print(f"\nEstimated P(X2 > 1 | do(X1 = 1.1*X1): {probability:.4f}")
+    print(f"\nEstimated P(X2 > 1 | do(X1 = 1.1*X1): {probability:.4f}")
